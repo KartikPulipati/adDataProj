53943f3f273a2ea343dc93ca91ae53013a04b54e
KunalSinha1125 <ksinha2@stanford.edu>
2021-11-26 12:05:34 -0800

    Create models for owner

--- owner/models.py
+++ owner/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+MAX_NAME_LENGTH = 200
+MAX_TITLE_LENGTH = 200
+
 # Create your models here.
+class Business(models.Model):
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+
+class Video(models.Model):
+    file = models.FileField(upload_to='videos')
+    title = models.CharField(max_length=MAX_TITLE_LENGTH)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    upload_date = models.DateField(auto_now_add=True)
+    num_views = models.IntegerField()
+
