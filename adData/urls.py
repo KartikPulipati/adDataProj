c33f302f742d16bc7ecda2b88ffe9511b9578d9f
Kartik M.P <Kartik.pulipati@gmail.com>
2021-11-26 10:26:41 -0800

    Setup signup stuff for rater

--- adData/urls.py
+++ adData/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from display import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('rater.urls')),
+    path('', views.home, name="home"),
 ]
--- rater/views.py
+++ rater/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def signUp(request):
+    return render(request, 'rater/signup.html')
