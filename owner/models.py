604cd8cde9012619579d7779147d80bd328cf9b8
KunalSinha1125 <ksinha2@stanford.edu>
2021-11-26 15:04:35 -0800

    Create tests for owners

--- owner/models.py
+++ owner/models.py
@@ -9,11 +9,16 @@ class Business(models.Model):
     name = models.CharField(max_length=MAX_NAME_LENGTH)
 
 class Video(models.Model):
+
     file = models.FileField(upload_to='videos')
     title = models.CharField(max_length=MAX_TITLE_LENGTH)
     uploader = models.ForeignKey(Business, on_delete=models.CASCADE)
     upload_date = models.DateField(auto_now_add=True)
     num_views = models.IntegerField()
+
+    def get_title(self):
+        print(self.title)
+        return self.title
 
 class Tag(models.Model):
     category = models.CharField(max_length=MAX_TITLE_LENGTH)
--- owner/tests.py
+++ owner/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from owner.models import Video, Business, Tag
 
 # Create your tests here.
+class BasicTestCase(TestCase):
+    def set_up(self):
+        Video.objects.create(title='epic', num_views=5)
+    def test_if_title_works(self):
+        epic = Video.objects.get(title='epic')
+        self.assertEqual(epic.get_title(), 'epic')
+
