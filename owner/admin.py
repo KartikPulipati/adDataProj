7929d8b9e9d61401ac381532b9b0f38a4091b57b
KunalSinha1125 <ksinha2@stanford.edu>
2021-11-26 18:27:54 -0800

    Comment out admin registrations

--- owner/admin.py
+++ owner/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Business)
-admin.site.register(models.Video)
-admin.site.register(models.Tag)
+#admin.site.register(models.Business)
+#admin.site.register(models.Video)
+#admin.site.register(models.Tag)
