0056eb0daf8e5979e9349ed8f7fa64fcc97bb2e9
KunalSinha1125 <ksinha2@stanford.edu>
2021-11-27 10:36:53 -0800

    Create Advertisement model

--- owner/models.py
+++ owner/models.py
@@ -1,4 +1,17 @@
 from django.db import models
-from rater.models import User
+from rater.models import rater
+
+MAX_TITLE_LENGTH = 200
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=MAX_TITLE_LENGTH)
+    media_file = models.FileField(upload_to='media/')
+    uploader = models.OneToOneField(Business)
+    upload_date = models.DateField(auto_add_now=True)
+    reward_per_question = models.FloatField()
+    viewers = models.ManyToManyField(rater)
+    num_views = models.IntegerField()
+    #is_done = models.BooleanField()
+
 
 
