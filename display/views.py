b67d0f25030b34273794957ec8cdff6120ab1bd3
Kartik M.P <Kartik.pulipati@gmail.com>
2021-12-22 07:26:01 -0800

    Added stuff so that u can't see form if u filled it out already

--- display/views.py
+++ display/views.py
@@ -10,20 +10,26 @@ def home(request):
 
 @login_required
 def viewAd(request, advertisement_pk):
+    notThere = True
     ad = get_object_or_404(advertisement, pk=advertisement_pk)
     ans = answer.objects.filter(ad=ad)
-    if request.method == 'POST':
-        form = responseForm(request.POST)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.user = request.user.rater
-            instance.ad = ad
-            ad.num_views = ad.num_views + 1
-            ad.save()
-            request.user.rater.credits = request.user.rater.credits + ad.reward
-            request.user.rater.save()
-            instance.save()
-            return redirect('discover')
-    else:
-        form = responseForm()
-    return render(request, 'display/viewAd.html', {'form': form, 'ad': ad, 'ans': ans})
+    for i in ans:
+        if i.user == request.user.rater:
+            notThere = False
+    if notThere:
+        if request.method == 'POST':
+            form = responseForm(request.POST)
+            if form.is_valid():
+                instance = form.save(commit=False)
+                instance.user = request.user.rater
+                instance.ad = ad
+                ad.num_views = ad.num_views + 1
+                ad.save()
+                request.user.rater.credits = request.user.rater.credits + ad.reward
+                request.user.rater.save()
+                instance.save()
+                return redirect('discover')
+        else:
+            form = responseForm()
+        return render(request, 'display/viewAd.html', {'form': form, 'ad': ad, 'ans': ans})
+    return render(request, 'display/viewAd.html', {'ad': ad, 'ans': ans})
